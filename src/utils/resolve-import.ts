972083357705cd2d93412b23d511e5d373f88f66
Menci <huanghaorui301@gmail.com>
2022-10-22 14:12:10 +0800

    fix: detecting imports from the parent directory (#12)

--- src/utils/resolve-import.ts
+++ src/utils/resolve-import.ts
@@ -2,7 +2,7 @@ import { posix as path } from "path";
 
 export function resolveImport(base: string, imported: string) {
   // Skip external packages
-  if (!imported.startsWith("./")) return null;
+  if (!(imported.startsWith("./") || imported.startsWith("../"))) return null;
 
   return path.join(path.dirname(base), imported);
 }
